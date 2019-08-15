e594160d91720953a34da8b3b116567ead46ea22
嘻嘻嘻 <930201676@qq.com>
2019-08-15 16:34:09 +0800

    Add files via upload

---
+++ xsshunter.api.start.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+cd /opt/xsshunter/api
+source /opt/xsshunter/api/env/bin/activate 
+/opt/xsshunter/api/apiserver.py
---
+++ xsshunter.gui.start.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+cd /opt/xsshunter/gui
+source /opt/xsshunter/gui/env/bin/activate 
+/opt/xsshunter/gui/guiserver.py
